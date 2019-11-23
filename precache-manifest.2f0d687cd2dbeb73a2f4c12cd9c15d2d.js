c8dcff213680f93c868bf7d079d8f4c228d0df69
PaulSklv <paulsklvv@gmail.com>
2019-11-23 11:39:13 +0100

    Updates

---
+++ precache-manifest.2f0d687cd2dbeb73a2f4c12cd9c15d2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e22b0e96a36fedc53ac89b4df500686",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "67cc4a786fc3f4cf3bda",
+    "url": "/fcc-pomodoro-clock/static/js/2.7b262a91.chunk.js"
+  },
+  {
+    "revision": "3702c5de5e26ba1dc371",
+    "url": "/fcc-pomodoro-clock/static/js/main.85f87af6.chunk.js"
+  },
+  {
+    "revision": "c72c6e16df4f71202871",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.c9dcae41.js"
+  }
+]);
